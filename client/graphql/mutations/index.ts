2f45c6aab6ad61ceef48f6cba2484a07b483323e
Rostyslav Sokolov <rosty.sokolov@gmail.com>
2019-03-31 17:18:50 -0700

    Rename mutations imports

--- client/graphql/mutations/index.ts
+++ client/graphql/mutations/index.ts
@@ -1,5 +1,5 @@
-export { default as logIn } from './logIn.mutation';
-export { default as logOut } from './logOut.mutation';
-export { default as signUp } from './signUp.mutation';
+export { default as logIn } from './logIn';
+export { default as logOut } from './logOut.';
+export { default as signUp } from './signUp';
 export { default as updateName } from './updateName.mutation';
 export { default as updateSettings } from './updateSettings.mutation';
