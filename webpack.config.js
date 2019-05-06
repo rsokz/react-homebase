c21821a50159c51661970fad0d2cebf96826d25a
Rostyslav Sokolov <rosty.sokolov@gmail.com>
2019-05-05 18:23:34 -0700

    Updated webpack file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: require.resolve('file-loader') + '?name=../[path][name].[ext]',
             options: {}
           }
         ]
